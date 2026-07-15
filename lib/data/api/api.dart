80fc97765412d6ee6c3925703a00221aca26927b
marrooov <merogamer999@gmail.com>
2026-07-16 02:15:15 +0300

    feat(api) : creating models

--- lib/data/api/api.dart
+++ lib/data/api/api.dart
@@ -1,17 +1,18 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:news_app/data/models/news_model.dart';
 
 abstract class Api{
   //https://newsapi.org/v2/everything?q=bitcoin&apiKey=09338c5143bb43bd8d8e36226c4bd5c1
-  static Future<Map<String, dynamic>> getNews() async {
+  static Future<NewsModel> getNews() async {
     Uri url = Uri.https('newsapi.org', '/v2/everything', {
       'q': 'bitcoin',
        'apiKey': '09338c5143bb43bd8d8e36226c4bd5c1'
        });
        var response = await http.get(url);
        String responseBody = response.body;
-       Map<String, dynamic> json = jsonDecode(responseBody);
-       return json;
+       var json = jsonDecode(responseBody);
+       return NewsModel.fromJson(json);
   }
 }
