565863edb82cbc56a18d616f343caed96731152d
marrooov <merogamer999@gmail.com>
2026-07-16 01:33:56 +0300

    feat(api): string & dynamic adding to the api's in the home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:news_app/data/api.dart';
 import 'package:news_app/widgets/image_item_widget.dart';
@@ -9,7 +10,6 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
-
 class _HomeScreenState extends State<HomeScreen> {
   Map<String, dynamic> data = {
         "status": "ok",
@@ -34,15 +34,44 @@ class _HomeScreenState extends State<HomeScreen> {
            ),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return ImageItemWidget(
-            image: data['articles'][index]['urlToImage'] ?? dummyImage,
-            title: data['articles'][index]['title'] ?? "Dynamic Title $index",
-            onTap: () {},
-          );
+      body: FutureBuilder(
+        future: Api.getNews(),
+        builder: (context, snapshot) {
+          log('future builder called');
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                'Error from call get news try again later',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.red, fontSize: 30),
+              ),
+            );
+          }
+          if (snapshot.hasData) {
+            log('future builder has data');
+            List<dynamic> data = snapshot.data?['articles'] ?? [];
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                return ImageItemWidget(
+                  image: data[index]['urlToImage'] ?? dummyImage,
+                  title: data[index]['title'] ?? "Empty String $index",
+                  onTap: () {},
+                );
+              },
+              itemCount: data.length,
+            );
+          } else {
+            return Center(
+              child: Text(
+                'No data available',
+                style: TextStyle(color: Colors.blue, fontSize: 20),
+              ),
+            );
+          }
         },
-        itemCount: (data['articles'] as List).length,
       ),
     );
   }
@@ -51,8 +80,9 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       this.data = data;
     });
+         }
   }
-}
+
 
 String dummyImage =
     'https://images.theconversation.com/files/651621/original/file-20250226-32-jxjhmy.jpg?ixlib=rb-4.1.0&rect=0%2C0%2C5991%2C3997&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip';
